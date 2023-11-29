cc2a2d2c7c8044729ca8f993384a5436d42121b5
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2023-11-30 01:29:55 +0600

    1st

---
+++ android/app/src/main/kotlin/com/example/icon_badge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.icon_badge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: Badge(
+          label: Text('10'),
+            backgroundColor: Colors.blue,
+            child: Icon(
+              Icons.notification_important_rounded,size: 50,),
+        ),
+      ),));
+  }
+}
